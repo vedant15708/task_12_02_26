584085fad7f1033979739d608c9ecee6bdf21a00
vedant15708 <vedant15708@gmail.com>
2026-02-13 11:39:39 +0530

    movie card gridview demo

---
+++ lib/model/movie_model.dart
@@ -0,0 +1,17 @@
+class MovieModel {
+  final String title;
+  final String genre;
+  final String image;
+  final String releaseDate;
+  final String runtime;
+  final double rating;
+
+  MovieModel({
+    required this.title,
+    required this.genre,
+    required this.image,
+    required this.releaseDate,
+    required this.runtime,
+    required this.rating,
+  });
+}
